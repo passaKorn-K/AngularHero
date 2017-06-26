95282d9940c2245994e0737a348b32b036f708c1
passaKorn-K <jakkajee380@gmail.com>
2017-06-26 07:37:38 +0700

    Complete routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './hero.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-    {
-      path: 'heroes',
-      component: HeroesComponent
-    }
-    ])
+    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './heroes.component.html',
+  styleUrls: [ './heroes.component.css' ]
+})
+
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(
+    private heroService: HeroService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+  this.router.navigate(['/detail', this.selectedHero.id]);
+}
+
+}
